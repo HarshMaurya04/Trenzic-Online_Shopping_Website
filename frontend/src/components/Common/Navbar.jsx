2aed189cec57853a474df34d665e69c27e182641
HarshMaurya04 <harshm8540@gmail.com>
2025-06-01 01:24:43 +0530

    Topbar and Navbar

---
+++ frontend/src/components/Common/Navbar.jsx
@@ -0,0 +1,68 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faBagShopping, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="container px-10 flex items-center justify-between py-6">
+      {/* Left - Logo */}
+      <div>
+        <Link to="/" className="text-3xl font-medium">
+          Trenzic
+        </Link>
+      </div>
+
+      {/* Center - Navigation Links */}
+      <div className="hidden md:flex space-x-6">
+        <Link
+          to="#"
+          className="text-gray-600 hover:text-black hover:underline text-sm font-medium uppercase"
+        >
+          Men
+        </Link>
+        <Link
+          to="#"
+          className="text-gray-600 hover:text-black hover:underline text-sm font-medium uppercase"
+        >
+          Women
+        </Link>
+        <Link
+          to="#"
+          className="text-gray-600 hover:text-black hover:underline text-sm font-medium uppercase"
+        >
+          Top Wear
+        </Link>
+        <Link
+          to="#"
+          className="text-gray-600 hover:text-black hover:underline text-sm font-medium uppercase"
+        >
+          Bottom Wear
+        </Link>
+      </div>
+
+      {/* Right - Icons */}
+      <div className="flex items-center gap-3">
+        <Link to="/profile" className="text-gray-600 hover:text-black">
+          <FontAwesomeIcon icon={faUser} className="h-6 w-6" />
+        </Link>
+
+        <button className="relative text-gray-600 hover:text-black">
+          <FontAwesomeIcon
+            icon={faBagShopping}
+            className="h-6 w-6"
+          />
+          <span className="absolute -top-1 -right-2 bg-[#d97706] text-white text-xs rounded-full px-1.5 py-0.5 ">4</span>
+        </button>
+
+        {/* Search Icon*/}
+
+
+        <button className="md:hidden text-gray-600 hover:text-black">
+          <FontAwesomeIcon icon={faBars} className="h-6 w-6"/>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      {/* Footer */}
+    </>
+  );
+};
+
+export default UserLayout;
