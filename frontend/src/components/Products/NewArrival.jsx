1a39c76b57b5cfa4d74ce964a55987c197082b8b
HarshMaurya04 <harshm8540@gmail.com>
2025-07-31 23:34:19 +0530

    Redux done

--- frontend/src/components/Products/NewArrival.jsx
+++ frontend/src/components/Products/NewArrival.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 const NewArrival = () => {
   const scrollRef = useRef(null);
@@ -11,96 +12,21 @@ const NewArrival = () => {
   const [canScrollLeft, setCanScrollLeft] = useState(false);
   const [canScrollRight, setCanScrollRight] = useState(true);
 
-  const newArrival = [
-    {
-      _id: "1",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=1",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "2",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=2",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "3",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=3",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "4",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=4",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "5",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=5",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "6",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=6",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "7",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=7",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-    {
-      _id: "8",
-      name: "Stylish Jacket",
-      price: 1200,
-      image: [
-        {
-          url: "https://picsum.photos/500/500?random=8",
-          altText: "Stylish Jacket",
-        },
-      ],
-    },
-  ];
+  const [newArrival, setNewArrivals] = useState([]);
+
+  useEffect(() => {
+    const fetchNewArrivals = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/products/new-arrivals`
+        );
+        setNewArrivals(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchNewArrivals();
+  }, []);
 
   const handleMouseDown = (e) => {
     setIsDragging(true);
@@ -145,7 +71,7 @@ const NewArrival = () => {
       updateScrollButtons();
       return () => container.removeEventListener("scroll", updateScrollButtons);
     }
-  }, []);
+  }, [newArrival]);
 
   return (
     <section>
@@ -153,7 +79,8 @@ const NewArrival = () => {
         <div className="text-center relative">
           <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
           <p className="text-lg text-grey-600 mb-16">
-            Stay ahead of the trends with our newest runway-inspired styles, curated to elevate your wardrobe.
+            Stay ahead of the trends with our newest runway-inspired styles,
+            curated to elevate your wardrobe.
           </p>
 
           {/* Scroll Buttons */}
@@ -201,8 +128,8 @@ const NewArrival = () => {
               className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative"
             >
               <img
-                src={product.image[0].url}
-                alt={product.image[0].altText || product.name}
+                src={product.images?.[0]?.url}
+                alt={product.images?.[0]?.altText || product.name}
                 className="w-full h-[500px] object-cover rounded-2xl"
                 draggable="false"
               />
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import login from "../assets/login.webp"
+import login from "../assets/login.webp";
+import { loginUser } from "../redux/slices/authSlice";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("User Login:", { email, password });
+    dispatch(loginUser({ email, password }));
   };
 
   return (
     <div className="flex">
       <div className="w-full md:w-1/2  flex flex-col justify-center items-center p-8 md:p-12 ">
-        <form onSubmit={handleSubmit} className="w-full max-w-md p-8 bg-white rounded-3xl border shadow-md">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md p-8 bg-white rounded-3xl border shadow-md"
+        >
           <div className="flex justify-center mb-6">
-            <h2 className="text-xl font-medium">Trenzic</h2> 
+            <h2 className="text-xl font-medium">Trenzic</h2>
           </div>
           <h2 className="text-2xl font-bold text-center mb-6">Hey there! 👋🏻</h2>
           <p className="text-center mb-6">
@@ -54,16 +60,22 @@ const Login = () => {
             Sign In
           </button>
           <p className="mt-6 text-center text-sm">
-            Don't have an account? {" "}
-            <Link to="/register" className="text-blue-500 underline">Register</Link>
+            Don't have an account?{" "}
+            <Link to="/register" className="text-blue-500 underline">
+              Register
+            </Link>
           </p>
         </form>
       </div>
 
       <div className="hidden md:block w-1/2 bg-gray-800">
-            <div className="h-full flex flex-col justify-center items-center">
-                <img src={login} alt="Login to Account" className="h-[750px] w-full object-cover"></img>
-            </div>
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src={login}
+            alt="Login to Account"
+            className="h-[750px] w-full object-cover"
+          ></img>
+        </div>
       </div>
     </div>
   );
